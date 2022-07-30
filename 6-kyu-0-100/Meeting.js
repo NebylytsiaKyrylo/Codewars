ba6ba83855cc3695592e55bf7a104f28539d1a58
Kyrylo Nebylytsia <knebylytsia.it@gmail.com>
2022-07-31 00:49:21 +0200

    Solution

--- 6-kyu-0-100/Meeting.js
+++ 6-kyu-0-100/Meeting.js
@@ -9,7 +9,8 @@ Could you make a program that
 
 makes this string uppercase
 gives it sorted in alphabetical order by last name.
-When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+When the last names are the same, sort them by first name.
+Last name and first name of a guest come in the result between parentheses separated by a comma.
 
 So the result of function meeting(s) will be:
 
