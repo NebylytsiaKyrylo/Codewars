4a277630495eacecb7136088fb6af70df961158e
NebylytsiaKyrylo <knebylytsia.it@gmail.com>
2022-12-28 14:39:47 +0100

    Solution

---
+++ 7-kyu-0-100/Divide and Conquer.js
@@ -0,0 +1,15 @@
+/*
+https://www.codewars.com/kata/57eaec5608fed543d6000021
+
+Given a mixed array of number and string representations of integers,
+add up the non-string integers and subtract this from the total of the string integers.
+
+Return as a number.
+*/
+
+const divCon = x =>
+  x.reduce((acc, cur) => typeof cur == 'number' ? acc + cur : acc - Number(cur), 0);
+
+console.log(divCon([9, 3, '7', '3'])); //2
+console.log(divCon(['5', '0', 9, 3, 2, 1, '9', 6, 7])); //14
+console.log(divCon(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0'])); //13
