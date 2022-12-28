7ea094ee3a5ad795fcad67876223e4599c04fbaa
NebylytsiaKyrylo <knebylytsia.it@gmail.com>
2022-12-28 14:42:07 +0100

    Solution

--- 7-kyu-0-100/Divide and Conquer.js
+++ 7-kyu-0-100/Divide and Conquer.js
@@ -10,6 +10,9 @@ Return as a number.
 const divCon = x =>
   x.reduce((acc, cur) => typeof cur == 'number' ? acc + cur : acc - Number(cur), 0);
 
+// const divCon = x =>
+//   x.reduce((acc, cur) => acc + (cur === +cur ? cur : -(+cur)), 0);
+
 console.log(divCon([9, 3, '7', '3'])); //2
 console.log(divCon(['5', '0', 9, 3, 2, 1, '9', 6, 7])); //14
 console.log(divCon(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0'])); //13
