0842af57ea06bf640b5e500fac33f1fe1061dcc6
NebylytsiaKyrylo <knebylytsia.it@gmail.com>
2022-12-29 17:36:06 +0100

    Solution

---
+++ 8-kyu-100-200/Basic variable assignment.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/50ee6b0bdeab583673000025
+
+This code should store "codewa.rs" as a variable called
+name but it's not working. Can you figure out why?
+*/
+
+const a = 'code',
+      b = 'wa.rs',
+      name = a + b;
+
+console.log(name);
