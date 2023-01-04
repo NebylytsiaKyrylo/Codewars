01d31a151f6875b09fa5c07e8eacf57cb9602772
NebylytsiaKyrylo <knebylytsia.it@gmail.com>
2023-01-04 14:56:42 +0100

    Solution

---
+++ 8-kyu-100-200/Hex to Decimal.js
@@ -0,0 +1,14 @@
+/*
+https://www.codewars.com/kata/57a4d500e298a7952100035d
+
+Complete the function which converts hex number
+(given as a string) to a decimal number.
+*/
+
+const hexToDec = hexString => parseInt(hexString, 16);
+
+console.log(hexToDec('1')); //1
+console.log(hexToDec('a')); //10
+console.log(hexToDec('10')); //16
+console.log(hexToDec('FF')); //255
+console.log(hexToDec('-C')); //-12
